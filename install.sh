d627de0cbf97415ef5e646de3213396b574c7ac9
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-05-11 22:08:46 -0700

    Add install script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Color pallet config file
+FILE=challenger-deep.dconf
+
+# dconf path for gnome-terminal
+TERM_CONFIG="/org/gnome/terminal/legacy/"
+
+# dconf path for default gnome-terminal profile
+PROFILE=$TERM_CONFIG"profiles:/default"
+
+# Updates target profile in config file
+function update_config {
+  REPLACE="[profiles:/:$1]"
+  REPLACE=$(echo $REPLACE | tr -d \'\")
+  sed -i "1c$REPLACE" $FILE
+}
+
+# Find default terminal profile
+ID=$(dconf read $PROFILE)
+
+# Set file to use profile
+update_config $ID
+
+# Load color pallet into terminal profile
+dconf load $TERM_CONFIG < $FILE
+
+# Reset config file
+update_config "'ID'"
